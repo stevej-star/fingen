c518ed9b1187330a8fb2bff8c264bfb13a21a332
Steve Jones <steve.jones@starlingbank.com>
2024-02-08 16:18:43 +0000

    add some more options and generate folder if not exists

--- fin-generator.js
+++ fin-generator.js
@@ -1,49 +1,80 @@
 import { v4 as uuidv4 } from 'uuid';
 import randomstring from 'randomstring';
 import dateFormat from "dateformat";
-import fs from 'node:fs/promises'; 
+import fs from 'fs';
 
 const failMessage = true
 const fromJpm = false
+const toBusinessAccount = false
+const closedAccount = true
 
-const jpmBic="CHASUS33"
-const natwestBic="NWBKGB2L"
-const starlingBic="SRLGGB2L"
+const jpmBic="CHASUS33XXX"
+const natwestBic="NWBKGB2LXXX"
+const starlingBic="SRLGGB2LXXX"
 const transactionReference="sj/" + randomstring.generate({length: 12});
 const transactionDate= dateFormat(new Date(), "yymmdd")
 const uetr= uuidv4()
-const currency="USD"
-const amount="10,99"
+const currency= fromJpm ? "USD" : "EUR" 
+const amount="101,99"
 const invalidBeneficaryAccount="GB92SRLG04004299943123"
-const validbeneficaryAccount="GB49SRLG04004254127174"
-const beneficaryName="No Account"
+const myDemoPersonalAcouunt="GB96SRLG04004272583973"
+const myDemoBusinessAccount="GB10SRLG04004232168725"
+const aClosedPersonalAccount="GB33SRLG04004289207997"
+const beneficaryAccount = getBeneficiary()
+const beneficaryName="Steve"
 const senderAccount=":60837105163870"
 const senderName="Dumbledore"
 const senderCountry="GB"
-const remittenceInfo="generated"
+const remittenceInfo="TestMessageGenerator"
 
-const finMessage = `{1:F01${starlingBic}XXX0000000000}{2:O1030000230831${fromJpm ? jpmBic : natwestBic}XXX76351234562308310000N}{3:{121:${uetr}}}{4:
+function getBeneficiary() {
+  if (toBusinessAccount) {
+    return myDemoBusinessAccount;
+  }
+  if (failMessage && closedAccount) {
+    return aClosedPersonalAccount;
+  }
+  if (failMessage) {
+    return invalidBeneficaryAccount;
+  }
+  return myDemoPersonalAcouunt;
+}
+
+function createDirectoryIfNotExists(folderName) {
+  if (!fs.existsSync(folderName)) {
+    fs.mkdirSync(folderName)
+  }
+}
+
+function writeFileToDirectory(filePath, finMessage) {
+  fs.writeFileSync(filePath, finMessage, err => {
+    if (err) {
+      console.log("Problem saving file: " + err);
+    }
+  });
+  console.log(`Saved file at: ${filePath}`)
+}
+
+
+const fileName = `fingen-${dateFormat(new Date(), "yymmdd-HHMMss")}.fin`
+const folderName = `/Users/steve.jones/code/finmessagegen/generated/${dateFormat(new Date(), "dddd-dd")}`
+const filePath = folderName + "/" + fileName;
+const finMessage = `{1:F01${starlingBic}0000000000}{2:O1030000230831${fromJpm ? jpmBic : natwestBic}76351234562308310000N}{3:{121:${uetr}}}{4:
 :20:${transactionReference}
 :23B:CRED
 :32A:${transactionDate}${currency}${amount}
 :33B:${currency}${amount}
 :50K:/${senderAccount}
 ${senderName}
-LONDON ENGLAND /${senderCountry}
+LONDON /${senderCountry}
 N1 7FQ
 :52A:${jpmBic}
-:59:/${failMessage ? invalidBeneficaryAccount : validbeneficaryAccount}
+:59:/${beneficaryAccount}
 ${beneficaryName}
 :70:${remittenceInfo}
 :71A:SHA
 -}
-`
+`;
 
-const fileName = `fingen-${dateFormat(new Date(), "yymmdd-HHMM")}.fin`
-const saveLocation = `/Users/steve.jones/code/finmessagegen/generated/${fileName}`
-fs.writeFile(saveLocation, finMessage, err => {
-  if (err) {
-    console.error(err);
-  }
-});
-console.log(`Saved ${fileName} in ${saveLocation}`)
+createDirectoryIfNotExists(folderName);
+writeFileToDirectory(filePath, finMessage);
