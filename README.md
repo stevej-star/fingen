cb97f57d4f069140ae450b273c5630a4a455194e
Steve Jones <steve.jones@starlingbank.com>
2024-02-08 11:03:19 +0000

    added basic readme and sample fin file

---
+++ README.md
@@ -0,0 +1,6 @@
+### Fin Message Generator
+
+Quick and simple script to generate valid fin messages with variable to set uneful fields. Got tired of manually editing and missing things. 
+
+Generates a random UETR and Transaction reference, also a file name based on the date and time.
+
--- fin-generator.js
+++ fin-generator.js
@@ -3,21 +3,26 @@ import randomstring from 'randomstring';
 import dateFormat from "dateformat";
 import fs from 'node:fs/promises'; 
 
-const receiverBic="SRLGGB2L"
-const senderBic="NWBKGB2L"
+const failMessage = true
+const fromJpm = false
+
+const jpmBic="CHASUS33"
+const natwestBic="NWBKGB2L"
+const starlingBic="SRLGGB2L"
 const transactionReference="sj/" + randomstring.generate({length: 12});
 const transactionDate= dateFormat(new Date(), "yymmdd")
 const uetr= uuidv4()
-const currency="GBP"
+const currency="USD"
 const amount="10,99"
-const beneficaryAccount="GB92SRLG04004207743328"
-const beneficaryName="Steve J"
+const invalidBeneficaryAccount="GB92SRLG04004299943123"
+const validbeneficaryAccount="GB49SRLG04004254127174"
+const beneficaryName="No Account"
 const senderAccount=":60837105163870"
 const senderName="Dumbledore"
 const senderCountry="GB"
-const remittenceInfo="generated from fin-generator.js"
+const remittenceInfo="generated"
 
-const finMessage = `{1:F01${senderBic}XXX0000000000}{2:O1030000230831${receiverBic}XXX76351234562308310000N}{3:{121:${uetr}}}{4:
+const finMessage = `{1:F01${starlingBic}XXX0000000000}{2:O1030000230831${fromJpm ? jpmBic : natwestBic}XXX76351234562308310000N}{3:{121:${uetr}}}{4:
 :20:${transactionReference}
 :23B:CRED
 :32A:${transactionDate}${currency}${amount}
@@ -26,16 +31,16 @@ const finMessage = `{1:F01${senderBic}XXX0000000000}{2:O1030000230831${receiverB
 ${senderName}
 LONDON ENGLAND /${senderCountry}
 N1 7FQ
-:52A:${senderBic}
-:59:/${beneficaryAccount}
+:52A:${jpmBic}
+:59:/${failMessage ? invalidBeneficaryAccount : validbeneficaryAccount}
 ${beneficaryName}
 :70:${remittenceInfo}
 :71A:SHA
 -}
 `
 
-const fileName = `fingen-${dateFormat(new Date(), "yymmdd-hhmmss")}.fin`
-const saveLocation = `/Users/steve.jones/code/finmessagegen/${fileName}`
+const fileName = `fingen-${dateFormat(new Date(), "yymmdd-HHMM")}.fin`
+const saveLocation = `/Users/steve.jones/code/finmessagegen/generated/${fileName}`
 fs.writeFile(saveLocation, finMessage, err => {
   if (err) {
     console.error(err);
